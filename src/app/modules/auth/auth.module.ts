a5f075aefab9b065fa40d12d8b9764299e62e15f
DESKTOP-3AAP87O\Rami Sonmez <lamasonmez.maids.cc@gmail.com>
2022-10-05 23:10:06 +0300

    add routing to auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthStoreModule } from './store/auth-store.module';
+import { LoginComponent } from './components/login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
+    //Routing
+    AuthRoutingModule,
     //Store
     AuthStoreModule
   ]
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,15 @@
 import {
     ActionReducerMap,
   } from '@ngrx/store';
+import * as fromAuth from '@auth/store/auth.reducer';
   
   export interface AppState {
-  
+    [fromAuth.authFeatureKey]: fromAuth.State;
    
   }
   
   export const reducers: ActionReducerMap<AppState> = {
-  
+    [fromAuth.authFeatureKey]: fromAuth.reducer,
     
   };
   
