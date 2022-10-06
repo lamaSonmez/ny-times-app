6f305f21462e1333a564ea862d0e60adffe637b5
DESKTOP-3AAP87O\Rami Sonmez <lamasonmez.maids.cc@gmail.com>
2022-10-06 21:27:05 +0300

    implement login page

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromAuthActions from '@auth/store/auth.actions';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,29 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  returnUrl:string='';
+  username:string='';
+  password:string='';
   constructor(
-    
-  ) { }
+    private _route:ActivatedRoute,
+    private _store:Store
+  ) { 
+
+  }
 
   ngOnInit(): void {
+    this._route.queryParamMap.subscribe(params=>this.returnUrl=params.get('returnUlr')||'');
     
+  }
+
+  onSubmit(): void {
+		this._store.dispatch(fromAuthActions.Login({
+      params:{
+        username:this.username,
+        password:this.password
+      },
+      returnUrl:this.returnUrl}));
+
   }
 
 }
